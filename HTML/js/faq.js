56986a7377d19dd2a40d6c96819ed17293eb263b
Dhrumil Mehta <59800321+dhrumilmehta@users.noreply.github.com>
2021-12-15 22:57:30 +0530

    faq prices submition

---
+++ HTML/js/faq.js
@@ -0,0 +1,97 @@
+let ans1=0;
+document.getElementById("faq-q1").onclick=function(){
+    ans1+=1;
+    if(ans1&1){
+        document.getElementById("faq-ar-1").src="img/faq/down-arrow.png";
+    }
+    else{
+        document.getElementById("faq-ar-1").src="img/faq/right-arrow.png";
+    }
+}
+let ans2=0;
+document.getElementById("faq-q2").onclick=function(){
+    ans2+=1;
+    if(ans2&1){
+        document.getElementById("faq-ar-2").src="img/faq/down-arrow.png";
+    }
+    else{
+        document.getElementById("faq-ar-2").src="img/faq/right-arrow.png";
+    }
+}
+
+let ans3=0;
+document.getElementById("faq-q3").onclick=function(){
+    ans1+=1;
+    if(ans1&1){
+        document.getElementById("faq-ar-3").src="img/faq/down-arrow.png";
+    }
+    else{
+        document.getElementById("faq-ar-3").src="img/faq/right-arrow.png";
+    }
+}
+
+let ans4=0;
+document.getElementById("faq-q4").onclick=function(){
+    ans1+=1;
+    if(ans1&1){
+        document.getElementById("faq-ar-4").src="img/faq/down-arrow.png";
+    }
+    else{
+        document.getElementById("faq-ar-4").src="img/faq/right-arrow.png";
+    }
+}
+
+let ans5=0;
+document.getElementById("faq-q5").onclick=function(){
+    ans1+=1;
+    if(ans1&1){
+        document.getElementById("faq-ar-5").src="img/faq/down-arrow.png";
+    }
+    else{
+        document.getElementById("faq-ar-5").src="img/faq/right-arrow.png";
+    }
+}
+
+let ans6=0;
+document.getElementById("faq-q6").onclick=function(){
+    ans6+=1;
+    if(ans6&1){
+        document.getElementById("faq-ar-6").src="img/faq/down-arrow.png";
+    }
+    else{
+        document.getElementById("faq-ar-6").src="img/faq/right-arrow.png";
+    }
+}
+
+let ans7=0;
+document.getElementById("faq-q7").onclick=function(){
+    ans1+=1;
+    if(ans1&1){
+        document.getElementById("faq-ar-7").src="img/faq/down-arrow.png";
+    }
+    else{
+        document.getElementById("faq-ar-7").src="img/faq/right-arrow.png";
+    }
+}
+
+let ans8=0;
+document.getElementById("faq-q8").onclick=function(){
+    ans1+=1;
+    if(ans1&1){
+        document.getElementById("faq-ar-8").src="img/faq/down-arrow.png";
+    }
+    else{
+        document.getElementById("faq-ar-8").src="img/faq/right-arrow.png";
+    }
+}
+
+let ans9=0;
+document.getElementById("faq-q9").onclick=function(){
+    ans1+=1;
+    if(ans1&1){
+        document.getElementById("faq-ar-9").src="img/faq/down-arrow.png";
+    }
+    else{
+        document.getElementById("faq-ar-9").src="img/faq/right-arrow.png";
+    }
+}
