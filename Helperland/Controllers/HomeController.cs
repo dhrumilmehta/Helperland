c8864a95a6de4f3732c7dbd57fb68702098c9655
Dhrumil Mehta <59800321+dhrumilmehta@users.noreply.github.com>
2022-01-25 07:18:43 +0530

    integration 1

---
+++ Helperland/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Helperland.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Helperland.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ViewResult faq()
+        {
+            return View();
+        }
+
+        public ViewResult prices()
+        {
+            return View();
+        }
+
+        public ViewResult about()
+        {
+            return View();
+        }
+        public ViewResult contact()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
