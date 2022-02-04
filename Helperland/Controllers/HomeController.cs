66746057ea0b4abc6226ca03cd0599866ccf40d5
Dhrumil Mehta <59800321+dhrumilmehta@users.noreply.github.com>
2022-02-04 23:30:09 +0530

    login commit

--- Helperland/Controllers/HomeController.cs
+++ Helperland/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Helperland.Models;
+using Helperland.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -11,34 +12,160 @@ namespace Helperland.Controllers
 {
     public class HomeController : Controller
     {
+
+        private readonly HelperlandContext _db = new HelperlandContext();
+
+       /* public HomeController(HelperlandContext db)
+        {
+            _db = db;
+        }*/
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
-
         public IActionResult Index()
         {
             return View();
         }
-        public ViewResult faq()
+        public IActionResult faq()
         {
             return View();
         }
 
-        public ViewResult prices()
+        public IActionResult prices()
         {
             return View();
         }
 
-        public ViewResult about()
+        public IActionResult about()
         {
             return View();
         }
-        public ViewResult contact()
+        public IActionResult contact()
         {
             return View();
+        }
+
+        public IActionResult become_Helper()
+        {
+            User user = new User();
+            return View(user);
+        }
+
+        public IActionResult SignupPage()
+        {
+            User user = new User();
+            return View(user);
+        }
+
+        //[ValidateAntiForgeryToken]
+        [HttpPost]
+        public IActionResult SignupPage(User user)
+        {
+
+            if (ModelState.IsValid)
+            {
+                if ((_db.Users.Where(x => x.Email == user.Email).Count() == 0 && _db.Users.Where(x => x.Mobile == user.Mobile).Count() == 0))
+                {
+
+                    user.CreatedDate = DateTime.Now;
+                    user.ModifiedDate = DateTime.Now;
+                    user.IsRegisteredUser = true;
+                    user.ModifiedBy = 123;
+                    user.UserTypeId = 1;
+
+                    _db.Users.Add(user);
+                    _db.SaveChanges();
+
+
+                    return RedirectToAction("Index");
+
+
+                }
+                else
+                {
+                    ViewBag.message = "User already exist.";
+                    return RedirectToAction("Index");
+
+
+                }
+            }
+            return View();
+
+        }
+        [HttpPost]
+        public IActionResult Become_Helper(User user)
+        {
+
+            if (ModelState.IsValid)
+            {
+                if ((_db.Users.Where(x => x.Email == user.Email).Count() == 0 && _db.Users.Where(x => x.Mobile == user.Mobile).Count() == 0))
+                {
+
+                    user.CreatedDate = DateTime.Now;
+                    user.ModifiedDate = DateTime.Now;
+                    user.IsRegisteredUser = true;
+                    user.ModifiedBy = 123;
+                    user.UserTypeId = 2;
+
+                    _db.Users.Add(user);
+                    _db.SaveChanges();
+
+
+                    return RedirectToAction("Index");
+
+
+                }
+                else
+                {
+                    ViewBag.message = "User already exist.";
+                    return RedirectToAction("Index");
+
+
+                }
+            }
+            return View();
+
+        }
+
+        [HttpPost]
+        public IActionResult Login(User user)
+        {
+
+            if (ModelState.IsValid)
+            {
+                if ((_db.Users.Where(x => x.Email == user.Email).Count() == 0 && _db.Users.Where(x => x.Password == user.Password).Count() == 0))
+                {
+                    ViewBag.message = "User invalid";
+                    return RedirectToAction("Index");
+
+
+                }
+                else
+                {
+                    ViewBag.message = "login successful";
+                    return RedirectToAction("contact");
+
+
+                }
+            }
+            return View();
+
+        }
+        [HttpPost]
+        public IActionResult Contact(ContactU contactU)
+        {
+
+            if (ModelState.IsValid)
+            {
+                contactU.IsDeleted = false;
+                return RedirectToAction("contact");
+            }
+            return View();
+
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
